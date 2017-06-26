0a25621eeaec596b13496a41b3fc5929a1176362
Kevin Coyle <kevin.coyle@drutek.co.uk>
2017-06-26 21:09:54 +0100

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style lang="sass">
+  @import '~assets/sass/variables';
+  @import '~assets/sass/main';
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+        Hello World
+    </section>
+</template>
+
