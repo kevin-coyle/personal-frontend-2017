b45db326e1f8bb034df05777b1acebf0cc657015
Kevin Coyle <kevin.coyle@drutek.co.uk>
2017-07-05 00:35:51 +0100

    Added title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'Kevin Coyle | Freelance Fullstack Drupal and Laravel Developer.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
