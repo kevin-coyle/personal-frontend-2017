fa66b52f567f5be5c07164508d15bdd16a521e36
Kevin Coyle <kevin.coyle@drutek.co.uk>
2017-07-06 12:32:27 +0100

    Added side images to blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -8,16 +8,24 @@
           <section class="section blog-listing">
           <h1 class="title is-2">Blog</h1>
           <ul>
-            <li v-for="post in posts">
+            <li v-for="(post,index) in posts">
             <nuxt-link :to="{ name: 'blog-id', params: { id: post.attributes.field_fieldablepath.replace('/blog/','') } }">
               <article class="blog-listing__teaser blog-teaser">
-              <header>
-                <h1 class="title is-4">
-                  {{post.attributes.title}}
-                </h1>
-                <time class="dateline">{{post.attributes.created | moment}}</time>
-              </header>
-              {{post.attributes.body.summary}}
+              <div class="columns is-mobile">
+                <div class="column is-one-quarter">
+                <img v-bind:src="included[index].attributes.url" v-bind:alt="post.relationships.field_side.data.meta.alt">
+                </div>
+                <div class="column">
+                  <header>
+                    <h1 class="title is-4">
+                      {{post.attributes.title}}
+                    </h1>
+                    <time class="dateline">{{post.attributes.created | moment}}</time>
+                  </header>
+                {{post.attributes.body.summary}}
+                </div>
+              </div>
+
               </article>
               </nuxt-link>
             </li>
@@ -45,10 +53,16 @@
       }
     },
     asyncData ({req, params}) {
-      return axios.get('https://backend.webdev.pro/jsonapi/node/blog')
+      return axios.get('https://backend.webdev.pro/jsonapi/node/blog', {
+        params: {
+          include: 'field_side,field_side.data'
+        }
+      })
           .then((res) => {
+            console.log(res)
             return {
-              posts: res.data.data
+              posts: res.data.data,
+              included: res.data.included
             }
           })
     }
