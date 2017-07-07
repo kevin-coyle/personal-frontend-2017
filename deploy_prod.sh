72faf7ebe598d55dc38c7dc6fe72c5d5649329c4
Kevin Coyle <kevin.coyle@drutek.co.uk>
2017-07-08 00:55:04 +0100

    Changed place for deployment to prod

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,2 +1,2 @@
 echo  "Syncing files to server"
-rsync -avzh dist/ s221267.gridserver.com@s221267.gridserver.com:domains/beta.webdev.pro/html/
+rsync -avzh dist/ s221268.gridserver.com@s221267.gridserver.com:domains/webdev.pro/html/
