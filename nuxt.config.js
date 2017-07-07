5f4946643b46c3c03d686917de6ef8e13bbbd872
Kevin Coyle <kevin.coyle@drutek.co.uk>
2017-07-08 00:52:51 +0100

    Added contact form

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,8 @@ module.exports = {
     '@nuxtjs/font-awesome'
   ],
   plugins: [
-    { src: '~plugins/ga.js', ssr: false}
+    { src: '~plugins/ga.js', ssr: false},
+    { src: '~plugins/jotform.js', ssr: false}
   ],
   css: [
     'normalize.css/normalize.css'
---
+++ pages/contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-contact">
+  <Heading></Heading>
+  <main>
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+        <section class="section contact-form">
+        <h1 class="title is-2">Contact Me</h1>
+          <iframe id="JotFormIFrame-71877642911363" onload="window.parent.scrollTo(0,0)" allowtransparency="true" src="https://form.jotformeu.com/71877642911363" frameborder="0" style="width:100%; height:539px; border:none;" scrolling="no"> </iframe>
+        </section>
+        </div>
+      </div>
+    </div>
+  </main>
+  </div>
+</template>
+<script>
+  import Heading from '~components/Heading.vue'
+
+  export default {
+    components: {
+      Heading
+    }
+  }
+
+</script>
